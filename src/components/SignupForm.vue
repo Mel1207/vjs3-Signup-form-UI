bb36babcfd2b8ca17a8293eba977eb5fc46d56f0
Mel <meljohnpualon@gmail.com>
2022-01-30 18:32:04 +0800

    added role form input

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,10 +5,16 @@
         <label>Password: </label>
         <input type="password" required v-model="password">
 
-        
+        <label>Role:</label>
+        <select v-model="role">
+            <option value="Backend Developer">Backend Developer</option>
+            <option value="Frontend Developer">Frontend Developer</option>
+            <option value="UI/UX Designer">UI/UX Designer</option>
+        </select>
     </form>
     <p>email: {{ email }}</p>
     <p>password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -16,7 +22,8 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: ''
         }
     },
     methods: {
