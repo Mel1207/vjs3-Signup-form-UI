7fb20fcc5e3b7bd40e36a3816cb5cd7fea935762
Mel <meljohnpualon@gmail.com>
2022-01-30 17:10:32 +0800

    uploaded signup form ui and first v-model role function

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>User Signup Form</h1>
+  <SignupForm />
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from './components/SignupForm.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {}
+  },
+  components: { SignupForm },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <label>Email:</label>
+        <input type="email" required v-model="email">
+        <label>Password: </label>
+        <input type="password" required v-model="password">
+
+        
+    </form>
+    <p>email: {{ email }}</p>
+    <p>password: {{ password }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
