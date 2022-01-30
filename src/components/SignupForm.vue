40705b2ca4917908fc1a527250b6483fd2f0b2ff
Mel <meljohnpualon@gmail.com>
2022-01-30 19:52:47 +0800

    added checkbox input fields

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,10 +11,16 @@
             <option value="Frontend Developer">Frontend Developer</option>
             <option value="UI/UX Designer">UI/UX Designer</option>
         </select>
+        <div class="terms">
+            <input type="checkbox" required v-model="terms">
+            <label>Accept Terms and conditions</label>
+        </div>
     </form>
+   
     <p>email: {{ email }}</p>
     <p>password: {{ password }}</p>
     <p>Role: {{ role }}</p>
+    <p>Terms accepted: {{ terms }}</p>
 </template>
 
 <script>
@@ -23,7 +29,8 @@ export default {
         return {
             email: '',
             password: '',
-            role: ''
+            role: '',
+            terms: false,
         }
     },
     methods: {
